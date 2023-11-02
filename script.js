dcf015f92911d7ede3b34cae9c6929e26c2d5a19
Masoud Ramezani <masoudhra@gmail.com>
2023-11-02 15:59:04 +1300

    react 2023 arrow function

---
+++ script.js
@@ -0,0 +1,18 @@
+// function duplicate(number){
+//     const result = number * 2;
+//     return result;
+// }
+
+// console.log(duplicate(10))
+
+// Fira code is a name of font
+
+// Arrow function
+
+const duplicate = (number) => number * 2;
+
+console.log(duplicate(10));
+// agar yek voroodi dashte bashim mitoonim nazarim parantezo
+// agar vorooddi nadashte bashim paranteze khali mizarim
+// baraye func haye chand khati az {} esteffade mikonim va cod ra minevisim
+
