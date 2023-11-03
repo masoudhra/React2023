891f7eed2f491f935a44ddac227a93f202655821
Masoud Ramezani <masoudhra@gmail.com>
2023-11-03 15:13:20 +1300

    filter - find

--- script.js
+++ script.js
@@ -20,3 +20,15 @@ console.log(duplicate(10));
 // const arr = [1, 2, 3];
 // const newArr = arr.map((item) => item * 2);
 // console.log(newArr)
+
+// filter - find
+const numbers = [22, 3, 43, 65, 7];
+const newNumbers = numbers.filter(item => item > 10);
+console.log(newNumbers);
+
+const number = [22, 3, 43, 65, 7];
+const newNumber = number.find(item => item > 10);
+console.log(newNumber);
+// avalin sharti ke true hast ro mide 
+
+
