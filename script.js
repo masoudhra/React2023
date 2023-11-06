9507b18901f90d7239c8bdbd19a316b98d9f2458
Masoud Ramezani <masoudhra@gmail.com>
2023-11-06 13:10:24 +1300

    template literals

--- script.js
+++ script.js
@@ -9,9 +9,9 @@
 
 // Arrow function
 
-const duplicate = (number) => number * 2;
+// const duplicate = (number) => number * 2;
 
-console.log(duplicate(10));
+// console.log(duplicate(10));
 // agar yek voroodi dashte bashim mitoonim nazarim parantezo
 // agar vorooddi nadashte bashim paranteze khali mizarim
 // baraye func haye chand khati az {} esteffade mikonim va cod ra minevisim
@@ -22,13 +22,29 @@ console.log(duplicate(10));
 // console.log(newArr)
 
 // filter - find
-const numbers = [22, 3, 43, 65, 7];
-const newNumbers = numbers.filter(item => item > 10);
-console.log(newNumbers);
+// const numbers = [22, 3, 43, 65, 7];
+// const newNumbers = numbers.filter(item => item > 10);
+// console.log(newNumbers);
 
-const number = [22, 3, 43, 65, 7];
-const newNumber = number.find(item => item > 10);
-console.log(newNumber);
+// const number = [22, 3, 43, 65, 7];
+// const newNumber = number.find(item => item > 10);
+// console.log(newNumber);
 // avalin sharti ke true hast ro mide 
 
+// reduce
+// kolle arayaro tabdil mikone beyek meghdar
+// const numbers = [2, 3, 4, 6, 7];
+// const result = numbers.reduce(
+//     (acc, cur) => acc + cur, 
+//     0
+//     );
+//     console.log(result)
+
+
+//template literals:
+const name = "Masoud";
+const age= 33;
+
+const text = `Hi I am ${name} and I am ${age} years old`;
+console.log(text);
 
